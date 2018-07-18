7a33e87875433850c71d65fc0b1e877ce135add1
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2018-07-18 11:02:41 +0530

    Added remaining icons, added progress bar, added homepage, simplified codes. Justdial, Share link, Rate app remaining.

--- app/src/main/java/com/example/abhishek/androidmanifesterweb/MainActivity.java
+++ app/src/main/java/com/example/abhishek/androidmanifesterweb/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,24 +14,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        webView=findViewById(R.id.webvw);
+        progressBar=findViewById(R.id.progress);
+        progressBar.setMax(100);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new Callback());
+
+
+        webView.loadUrl("https://www.pinderful.net/uploads/pins/2015/08/medium/6df9afed32f65c5231ed810a00699c94.jpeg");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +74,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     @Override
     public void onBackPressed() {
@@ -112,47 +120,44 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(final MenuItem item) {
         // Handle navigation view item clicks here.
-        webView=findViewById(R.id.webvw);
+
         int id = item.getItemId();
+        webView.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged(WebView view,int progress){
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(progress);
+
+                setTitle("Loading...");
+
+                if (progress==100){
+                    progressBar.setVisibility(View.GONE);
+                    setTitle(item.getTitle());
+                }
+            }
+        });
 
         if (id == R.id.nav_facebook) {
-            Toast.makeText(this, "Loading Facebook", Toast.LENGTH_SHORT).show();
-            webView.setWebViewClient(new Callback());
             webView.loadUrl("https://www.facebook.com/pg/androidmanifester/reviews/");
-
         }
         else if (id == R.id.nav_linkedin) {
-            Toast.makeText(this, "Loading Linkedin", Toast.LENGTH_SHORT).show();
-            webView.setWebViewClient(new Callback());
-            webView.getSettings().setJavaScriptEnabled(true);
             webView.loadUrl("https://www.linkedin.com/recs/give?senderId=androidranjith");
-
         }
         else if (id == R.id.nav_google) {
-            Toast.makeText(this, "Loading Google", Toast.LENGTH_SHORT).show();
-            webView.setWebViewClient(new Callback());
             webView.loadUrl("https://g.co/kgs/HAQDhq");
         }
         else if (id == R.id.nav_justdial) {
-            Toast.makeText(this, "Loading Justdial", Toast.LENGTH_SHORT).show();
-            webView.setWebViewClient(new Callback());
-            webView.getSettings().setJavaScriptEnabled(true);
             webView.loadUrl("https://www.justdial.com/Chennai/Android-Manifester-Near-Mari-Hotel-Saidapet/044PXX44-XX44-170105133148-D9F4_BZDET?tab=writereview");
         }
         else if (id == R.id.nav_quora) {
-            Toast.makeText(this, "Loading Quora", Toast.LENGTH_SHORT).show();
-            webView.setWebViewClient(new Callback());
             webView.loadUrl("http://qr.ae/TUpgLo");
         }
         else if (id == R.id.nav_urbanpro) {
-            Toast.makeText(this, "Loading Urbanpro", Toast.LENGTH_SHORT).show();
             webView.loadUrl("https://www.urbanpro.com/providerRecommendation/fillRecommendation?branchId=613640&fromProfile=fromProfile");
         }
 
         else if (id == R.id.nav_website) {
-            Toast.makeText(this, "Loading Website", Toast.LENGTH_SHORT).show();
             webView.loadUrl("http://www.androidmanifester.com/");
         }
 
@@ -161,11 +166,14 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+
+
  class Callback extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        return (false);
+        view.loadUrl(url);
+        return true;
     }
 
 }
